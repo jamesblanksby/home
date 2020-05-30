8addd6e9f609c6b88260ca805954be0f9e930ebc
James Blanksby <james@thisismyengine.com>
2020-05-30 13:10:51 +0100

    Updated event create table command

--- docker/protect/database/conf/create_table_event.sql
+++ docker/protect/database/conf/create_table_event.sql
@@ -1,9 +1,9 @@
 CREATE TABLE `event` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `camera` INT(11) NOT NULL,
-  `file` VARCHAR(255) NOT NULL,
-  `time_insert` TIMESTAMP NULL,
-  `time_update` TIMESTAMP NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `camera` int(11) NOT NULL,
+  `threshold` int(11) NOT NULL DEFAULT '0',
+  `lock` tinyint(1) NOT NULL DEFAULT '0',
+  `time_insert` timestamp NULL DEFAULT NULL,
+  `time_update` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-);
-
+) DEFAULT CHARSET=utf8mb4;
